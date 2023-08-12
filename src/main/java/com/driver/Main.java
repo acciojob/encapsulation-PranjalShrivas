65879923e9c76a551e2328ee89fd9c66ee690ba8
Pranjal Shrivas <shrivas.5298@gmail.com>
2023-08-12 16:58:52 +0530

    First committ

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,10 @@
 package com.driver;
 
 public class Main {
-  
+  public static void main(String []args){
+      RWOnly obj=new RWOnly();
+//      obj.name="Pranjal";
+      obj.setName("Pranjal");
+      obj.getName();
+  }
 }
